245dfb7a6a40fe4e88963d9610de87ce34321bf6
arunkls <arunkls195@gmail.com>
2024-10-12 14:36:34 -0700

    deploy to build

---
+++ build/static/js/8.064b8112.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpportfolio-app"]=this["webpackJsonpportfolio-app"]||[]).push([[8],{65:function(t,e,o){"use strict";o.r(e);o(0),o(67);var p=o(1);e.default=function(t){const{title:e}=t;return Object(p.jsx)("div",{className:"header",children:e})}}}]);
+//# sourceMappingURL=8.064b8112.chunk.js.map
